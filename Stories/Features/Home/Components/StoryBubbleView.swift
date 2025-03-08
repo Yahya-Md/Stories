f32be35d2359531b585b358292cf4aecf19222b0
Yahya-Md <61594504+Yahya-Md@users.noreply.github.com>
2025-03-08 15:06:34 +0000

    Creating Story Bubble

---
+++ Stories/Features/Home/Components/StoryBubbleView.swift
@@ -0,0 +1,18 @@
+//
+//  StoryBubbleView.swift
+//  Stories
+//
+//  Created by Yahya on 8/3/2025.
+//
+
+import SwiftUI
+
+struct StoryBubbleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryBubbleView()
+}
