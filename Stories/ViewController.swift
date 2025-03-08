6d755295b21138d19d163533f4b686e50f6a4670
Yahya-Md <61594504+Yahya-Md@users.noreply.github.com>
2025-03-08 13:59:50 +0000

    Initial Commit

---
+++ Stories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stories
+//
+//  Created by Yahya on 8/3/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StoriesTests/StoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesTests.swift
+//  StoriesTests
+//
+//  Created by Yahya on 8/3/2025.
+//
+
+import Testing
+@testable import Stories
+
+struct StoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
