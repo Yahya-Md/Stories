9ae1467ccb4f3818612c2a0e870360559c7a9573
Yahya-Md <61594504+Yahya-Md@users.noreply.github.com>
2025-03-08 17:16:54 +0000

    Adding viewmodel

--- Stories/Features/Home/Presentation/ViewModels/StoriesViewModel.swift
+++ Stories/Features/Home/Presentation/ViewModels/StoriesViewModel.swift
@@ -4,5 +4,64 @@
 //
 //  Created by Yahya on 8/3/2025.
 //
+import Combine
 
-import Foundation
+final class StoriesViewModel: LoadableObject {
+    
+    var state: LoadingState<[Stories]> = .idle
+    var canLoadMore: Bool = false
+    var isLoadingMore: Bool = false
+    private var totalPages = 0
+    private var currentPage = 0
+    private var totalStories = [Stories]()
+    private var cancelables = Set<AnyCancellable>()
+    private let useCase: GetStoriesUseCase
+    
+    init(useCase: GetStoriesUseCase) {
+        self.useCase = useCase
+    }
+    
+    func load() {
+        state = .loading
+        useCase.execute(currentPage: currentPage).sink { [weak self] completion in
+            guard let self else { return }
+            handleCompletion(completion)
+        } receiveValue: { [weak self] result in
+            guard let self else { return }
+            totalPages = result.totalPages
+            totalStories = [result]
+            state = .loaded(totalStories)
+        }
+        .store(in: &cancelables)
+
+    }
+    
+    func loadMore() {
+        canLoadMore = currentPage < totalPages
+        guard !isLoadingMore && canLoadMore else { return }
+        isLoadingMore = true
+        useCase.execute(currentPage: currentPage + 1).sink { [weak self] completion in
+            guard let self else { return }
+            handleCompletion(completion)
+        } receiveValue: { [weak self] result in
+            guard let self else { return }
+            handleReceiveValue(result)
+        }
+        .store(in: &cancelables)
+    }
+
+    private func handleCompletion(_ completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .failure(let error):
+            state = .failed(error)
+        case .finished:
+            break
+        }
+    }
+    
+    private func handleReceiveValue(_ result: Stories) {
+        totalPages = result.totalPages
+        totalStories.append(result)
+        state = .loaded(totalStories)
+    }
+}
