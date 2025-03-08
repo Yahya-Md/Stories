178ae6b240631c0ad8f042d96de1b08da8b31b29
Yahya-Md <61594504+Yahya-Md@users.noreply.github.com>
2025-03-08 14:52:30 +0000

    Creating the AddStoryButton

---
+++ Stories/Features/Home/Components/AddStoryButton.swift
@@ -0,0 +1,57 @@
+//
+//  AddStoryButton.swift
+//  Stories
+//
+//  Created by Yahya on 8/3/2025.
+//
+
+import SwiftUI
+
+struct AddStoryButton: View {
+    let action: (() -> Void)
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            storyButtonLabel
+        }
+        .padding(.vertical)
+        .padding(.horizontal, 10)
+    }
+}
+
+private extension AddStoryButton {
+    @ViewBuilder
+    var storyButtonLabel: some View {
+        Text("YM")
+            .multilineTextAlignment(.center)
+            .padding(10)
+            .foregroundColor(.white)
+            .background(
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 50, height: 50)
+            )
+            .overlay {
+                plusImage
+            }
+    }
+    
+    @ViewBuilder
+    var plusImage: some View {
+        Image(systemName: "plus")
+            .background(
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 20, height: 20)
+                
+            )
+            .offset(x: 15, y: 20)
+    }
+}
+
+#Preview {
+    AddStoryButton {
+        
+    }
+}
--- Stories/Features/Home/Presentation/Views/HomeView.swift
+++ Stories/Features/Home/Presentation/Views/HomeView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HStack {                    ScrollView(.horizontal) {
+                        AddStoryButton { }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+        }
     }
+
 }
 
 #Preview {
